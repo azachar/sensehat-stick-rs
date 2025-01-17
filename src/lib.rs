7ce538c2f97cb0a2e1369d83778b20843fe5a390
Isaac Mills <rooster0055@protonmail.com>
2025-01-16 21:15:41 -0700

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,15 @@
 //! The Joystick provides a driver for `evdev`.
 #[cfg(feature = "linux-evdev")]
 extern crate evdev;
-#[macro_use]
-extern crate failure;
 extern crate glob;
-#[cfg(feature = "poll")]
-extern crate mio;
-
-#[cfg(feature = "poll")]
-mod poll;
 
 use evdev::Device;
-use failure::Error;
 use glob::glob;
+use num_enum::TryFromPrimitive;
 
 use std::io;
-use std::os::unix::io::RawFd;
-use std::time::Duration;
+use std::os::{fd::AsRawFd, unix::io::RawFd};
+use std::time::{Duration, UNIX_EPOCH};
 
 // Device name provided by the hardware. We match against it.
 const SENSE_HAT_EVDEV_NAME: &[u8; 31] = b"Raspberry Pi Sense HAT Joystick";
@@ -35,7 +28,8 @@ const SENSE_HAT_EVDEV_NAME: &[u8; 31] = b"Raspberry Pi Sense HAT Joystick";
 /// * `Direction::Down = 108`
 /// * `Direction::Left = 105`
 /// * `Direction::Up = 106`
-#[derive(Debug)]
+#[repr(usize)]
+#[derive(Debug, TryFromPrimitive)]
 pub enum Direction {
     Enter = 28,
     Up = 103,
@@ -44,45 +38,22 @@ pub enum Direction {
     Right = 106,
 }
 
-impl Direction {
-    fn try_from(code: usize) -> Result<Self, Error> {
-        match code {
-            c if c == Direction::Enter as usize => Ok(Direction::Enter),
-            c if c == Direction::Up as usize => Ok(Direction::Up),
-            c if c == Direction::Down as usize => Ok(Direction::Down),
-            c if c == Direction::Left as usize => Ok(Direction::Left),
-            c if c == Direction::Right as usize => Ok(Direction::Right),
-            c => bail!("unrecognized joystick code: {}", c),
-        }
-    }
-}
-
 /// The action that was executed with the given `Direction`.
-#[derive(Debug)]
+#[repr(usize)]
+#[derive(Debug, TryFromPrimitive)]
 pub enum Action {
     Release = 0,
     Press = 1,
     Hold = 2,
 }
 
-impl Action {
-    fn try_from(code: usize) -> Result<Self, Error> {
-        match code {
-            c if c == Action::Press as usize => Ok(Action::Press),
-            c if c == Action::Release as usize => Ok(Action::Release),
-            c if c == Action::Hold as usize => Ok(Action::Hold),
-            c => bail!("unrecognized joystick action: {}", c),
-        }
-    }
-}
-
 /// An event issued by the `JoyStick`. Provides a UNIX-timestamp in the form of
 /// `std::time::Duration`, the `Direction`, and the `Action` that were issued by the `JoyStick`.
 #[derive(Debug)]
 pub struct JoyStickEvent {
-    timestamp: Duration,
-    direction: Direction,
-    action: Action,
+    pub timestamp: Duration,
+    pub direction: Direction,
+    pub action: Action,
 }
 
 impl JoyStickEvent {
@@ -96,42 +67,45 @@ impl JoyStickEvent {
 }
 
 /// A type representing the Sense HAT joystick device.
-#[derive(Debug)]
 pub struct JoyStick {
     device: Device,
 }
 
 impl JoyStick {
     /// Open the joystick device by name in the `/dev/input/event*` path on the filesystem.
-    pub fn open() -> Result<Self, Error> {
-        for entry in glob("/dev/input/event*")? {
+    pub fn open() -> Result<Self, io::Error> {
+        for entry in glob("/dev/input/event*")
+            .map_err(|e| std::io::Error::new(io::ErrorKind::InvalidInput, e))?
+        {
             match entry {
                 Ok(path) => {
                     let device = Device::open(&path)?;
-                    if device.name().as_bytes() == SENSE_HAT_EVDEV_NAME {
+                    if device.name().unwrap_or_default().as_bytes() == SENSE_HAT_EVDEV_NAME {
                         return Ok(JoyStick { device });
                     }
                 }
-                Err(e) => return Err(e.into()),
+                Err(e) => return Err(e.into_error()),
             }
         }
-        bail!("No Joystick found")
+        return Err(std::io::Error::new(
+            io::ErrorKind::NotFound,
+            "No Joystick found",
+        ));
     }
 
     /// Returns a result with a `Vec<JoyStickEvent>`. This function will
     /// block the current thread until events are issued by the `JoyStick` device.
     pub fn events(&mut self) -> io::Result<Vec<JoyStickEvent>> {
-        let events: Vec<JoyStickEvent> = self.device
-            .events_no_sync()
+        let events: Vec<JoyStickEvent> = self
+            .device
+            .fetch_events()
             .map_err(|e| io::Error::from(e))?
-            .filter(|ev| ev._type == 1)
+            .filter(|ev| ev.event_type().0 == 1)
             .map(|ev| {
-                let secs = ev.time.tv_sec as u64;
-                let nsecs = ev.time.tv_usec as u32 * 1_000;
-                let time = Duration::new(secs, nsecs);
+                let time = ev.timestamp().duration_since(UNIX_EPOCH).unwrap();
 
-                let direction = Direction::try_from(ev.code as usize).unwrap();
-                let action = Action::try_from(ev.value as usize).unwrap();
+                let direction = Direction::try_from(ev.code() as usize).unwrap();
+                let action = Action::try_from(ev.value() as usize).unwrap();
                 JoyStickEvent::new(time, direction, action)
             })
             .collect();
@@ -140,6 +114,6 @@ impl JoyStick {
 
     /// Returns the raw file-descriptor, `RawFd`, for the the Joystick.
     pub fn fd(&self) -> RawFd {
-        self.device.fd()
+        self.device.as_raw_fd()
     }
 }
